3e3e86ce15677c00625c132d8143d33d1f3ca0d1
Ilya Emelyanov <baralgin38@yandex.ru>
2022-04-06 15:14:54 +0300

    Refactoring
    Inside the component, an extra function was removed that simply returned
    the react element.
    Now the react element is directly returned from the component

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -2,37 +2,33 @@ import React from "react";
 import User from "./user";
 
 const Users = (props) => {
-  const renderTable = () => {
-    if (props.usersData.length !== 0) {
-      return (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Профессия</th>
-              <th scope="col">Встретился, раз</th>
-              <th scope="col">Оценка</th>
-              <th scope="col">Избранное</th>
-              <th scope="col" />
-            </tr>
-          </thead>
-          <tbody>
-            {props.usersData.map((user) => (
-              <User
-                key={user._id}
-                user={user}
-                onDelete={props.onDelete}
-                onClickBookmark={props.onClickBookmark}
-              />
-            ))}
-          </tbody>
-        </table>
-      );
-    }
-  };
+  if (props.usersData.length === 0) return null;
 
-  return <>{renderTable()}</>;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Имя</th>
+          <th scope="col">Качества</th>
+          <th scope="col">Профессия</th>
+          <th scope="col">Встретился, раз</th>
+          <th scope="col">Оценка</th>
+          <th scope="col">Избранное</th>
+          <th scope="col" />
+        </tr>
+      </thead>
+      <tbody>
+        {props.usersData.map((user) => (
+          <User
+            key={user._id}
+            user={user}
+            onDelete={props.onDelete}
+            onClickBookmark={props.onClickBookmark}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default Users;
