828b9d2f204588795e9fb834076349e5671398ef
Ilya Emelyanov <baralgin38@yandex.ru>
2022-04-06 15:06:12 +0300

    Refactoring
    removes the wrong 'key' attribute from the returned react element

--- src/app/components/quality.jsx
+++ src/app/components/quality.jsx
@@ -1,25 +1,9 @@
 import React from "react";
 
 const Quality = (props) => {
-  // const qualities = props.qualities.map(({ _id, name, color }) => {
-  //   const badgeClass = `badge bg-${color} m-1`;
+  const { name, color } = props;
 
-  //   return (
-  //     <span key={_id} className={badgeClass}>
-  //       {name}
-  //     </span>
-  //   );
-  // });
-
-  // return qualities;
-
-  const { _id, name, color } = props;
-
-  return (
-    <span key={_id} className={`badge bg-${color} m-1`}>
-      {name}
-    </span>
-  );
+  return <span className={`badge bg-${color} m-1`}>{name}</span>;
 };
 
 export default Quality;
