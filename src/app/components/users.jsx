53f111a6362b7ec51871a93e8bfcd5ea690f6937
Ilya Emelyanov <baralgin38@yandex.ru>
2022-04-07 11:30:44 +0300

    installs 'lodash', adds a pagination component

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,33 +1,47 @@
 import React from "react";
+import Pagination from "./pagination";
 import User from "./user";
 
 const Users = (props) => {
-  if (props.usersData.length === 0) return null;
+  const count = props.usersData.length;
+  const pageSize = 4;
+  const handlePageChange = (pageIndex) => {
+    console.log("page: ", pageIndex);
+  };
+
+  if (count === 0) return null;
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Имя</th>
-          <th scope="col">Качества</th>
-          <th scope="col">Профессия</th>
-          <th scope="col">Встретился, раз</th>
-          <th scope="col">Оценка</th>
-          <th scope="col">Избранное</th>
-          <th scope="col" />
-        </tr>
-      </thead>
-      <tbody>
-        {props.usersData.map((user) => (
-          <User
-            key={user._id}
-            user={user}
-            onDelete={props.onDelete}
-            onClickBookmark={props.onClickBookmark}
-          />
-        ))}
-      </tbody>
-    </table>
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+            <th scope="col">Избранное</th>
+            <th scope="col" />
+          </tr>
+        </thead>
+        <tbody>
+          {props.usersData.map((user) => (
+            <User
+              key={user._id}
+              user={user}
+              onDelete={props.onDelete}
+              onClickBookmark={props.onClickBookmark}
+            />
+          ))}
+        </tbody>
+      </table>
+      <Pagination
+        itemsCount={count}
+        pageSize={pageSize}
+        handlePageChange={handlePageChange}
+      />
+    </>
   );
 };
 
