bb2b7b890c1fbae36897a5d17b5de250057a7c11
Ilya Emelyanov <baralgin38@yandex.ru>
2022-04-20 17:23:32 +0300

    Adds a cursor change
    When hovering over the filtering parameters, the cursor changes

--- src/app/components/usersTable.jsx
+++ src/app/components/usersTable.jsx
@@ -7,20 +7,28 @@ const UsersTable = ({ users, onSort, ...rest }) => {
     <table className="table">
       <thead>
         <tr>
-          <th onClick={() => onSort('name')} scope="col">
+          <th onClick={() => onSort('name')} scope="col" role={'button'}>
             Имя
           </th>
           <th scope="col">Качества</th>
-          <th onClick={() => onSort('profession.name')} scope="col">
+          <th
+            onClick={() => onSort('profession.name')}
+            scope="col"
+            role={'button'}
+          >
             Профессия
           </th>
-          <th onClick={() => onSort('completedMeetings')} scope="col">
+          <th
+            onClick={() => onSort('completedMeetings')}
+            scope="col"
+            role={'button'}
+          >
             Встретился, раз
           </th>
-          <th onClick={() => onSort('rate')} scope="col">
+          <th onClick={() => onSort('rate')} scope="col" role={'button'}>
             Оценка
           </th>
-          <th onClick={() => onSort('bookmark')} scope="col">
+          <th onClick={() => onSort('bookmark')} scope="col" role={'button'}>
             Избранное
           </th>
           <th />
