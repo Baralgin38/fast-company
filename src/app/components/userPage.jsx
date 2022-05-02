bbd26052eae5e9b580263c6c785b283ff829eaac
Ilya Emelyanov <baralgin38@yandex.ru>
2022-05-02 13:20:59 +0300

    Implements the acquisition of user data
    The useState and useEffect hook has been added to the UserPage
    component. Using useEffect, an asynchronous request for user data is
    emulated, and the state when receiving data is updated and saves data to
    the component.

--- src/app/components/userPage.jsx
+++ src/app/components/userPage.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import api from '../api';
 
 const UserPage = ({ userId }) => {
-  return <h1>{`Я карточка пользователя с id:${userId}`}</h1>;
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    api.users.getUserById(userId).then((userData) => setUser(userData));
+  }, []);
+
+  console.log(user);
+  return (
+    <>
+      {user ? (
+        <h2>{`I'm ${user.name} with id: ${user._id}`}</h2>
+      ) : (
+        'Loading user data...'
+      )}
+    </>
+  );
 };
 
 export default UserPage;
