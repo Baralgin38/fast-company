c3b267bf075413906b02192b347740d6c7ff176a
Ilya Emelyanov <baralgin38@yandex.ru>
2022-05-02 12:48:24 +0300

    Creates UsersList component
    To implement the optional path, the UsersList component was created, all
    the code from the Users component was transferred to it. And the Users
    component itself now displays either UserPage or UsersList based on the
    conditions.

---
+++ src/app/components/usersList.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react';
+import Pagination from '../components/pagination';
+import UsersTable from '../components/usersTable';
+import { paginate } from '../utils/paginate';
+import GroupList from '../components/groupList';
+import SearchStatus from '../components/searchStatus';
+import api from '../api';
+import _ from 'lodash';
+
+const UsersList = () => {
+  const pageSize = 6;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [professions, setProfessions] = useState();
+  const [selectedProf, setSelectedProf] = useState();
+  const [sortBy, setSortBy] = useState({ iter: 'name', order: 'asc' });
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    api.users.fetchAll().then((data) => setUsers(data));
+  }, []);
+
+  const handleDelete = (id) => {
+    setUsers((prevState) => prevState.filter((user) => user._id !== id));
+  };
+
+  const handleToggleBookmark = (id) => {
+    const updatedState = users.map((user) => {
+      if (user._id === id) {
+        user.bookmark = !user.bookmark;
+      }
+
+      return user;
+    });
+
+    setUsers(updatedState);
+  };
+
+  useEffect(() => {
+    api.professions.fetchAll().then((data) => setProfessions(data));
+  }, []);
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [selectedProf]);
+
+  const handleProfessionSelect = (item) => {
+    setSelectedProf(item);
+  };
+
+  const handlePageChange = (pageIndex) => {
+    setCurrentPage(pageIndex);
+  };
+
+  const handleSortClick = (item) => {
+    setSortBy(item);
+  };
+
+  const clearFilter = () => {
+    setSelectedProf(undefined);
+  };
+
+  if (users) {
+    const filteredUsers = selectedProf
+      ? users.filter(
+          (user) =>
+            JSON.stringify(user.profession) === JSON.stringify(selectedProf)
+        )
+      : users;
+
+    const count = filteredUsers.length;
+    const sortedUsers = _.orderBy(filteredUsers, [sortBy.path], [sortBy.order]);
+    const userCrop = paginate(sortedUsers, currentPage, pageSize);
+
+    return (
+      <div className="d-flex">
+        {professions && (
+          <div className="d-flex flex-column flex-shrink-0 p-3">
+            <GroupList
+              items={professions}
+              onItemSelect={handleProfessionSelect}
+              selectedItem={selectedProf}
+            />
+            <button className="btn btn-secondary mt-2" onClick={clearFilter}>
+              Очистить
+            </button>
+          </div>
+        )}
+        <div className="d-flex flex-column">
+          <SearchStatus peopleNumber={count} />
+          {count > 0 && (
+            <UsersTable
+              users={userCrop}
+              onSort={handleSortClick}
+              selectedSort={sortBy}
+              onDelete={handleDelete}
+              onToggleBookmark={handleToggleBookmark}
+            />
+          )}
+          <div className="d-flex justify-content-center">
+            <Pagination
+              itemsCount={count}
+              itemsCountOnPage={userCrop.length}
+              pageSize={pageSize}
+              currentPage={currentPage}
+              onPageChange={handlePageChange}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return 'Loding...';
+};
+
+export default UsersList;
