4b29a2af1457fb2e0d742626163a38dd2a312fee
Ilya Emelyanov <baralgin38@yandex.ru>
2022-05-06 15:00:42 +0300

    Makes the email field a controlled component

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+
+  const handleChange = (evt) => {
+    setEmail(evt.target.value);
+  };
+
   return (
     <form action="">
       <div>
-        <label htmlFor="email">Email</label>
-        <input type="text" id="email" />
+        <label className="m-3" htmlFor="email">
+          Email
+        </label>
+        <input type="text" id="email" value={email} onChange={handleChange} />
       </div>
       <div>
-        <label htmlFor="password">Пароль</label>
+        <label className="m-3" htmlFor="password">
+          Пароль
+        </label>
         <input type="password" id="password" />
       </div>
     </form>
