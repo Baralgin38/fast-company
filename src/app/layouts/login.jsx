9b83cada0083e8594d69586cd70f9b8b1d8c14ef
Ilya Emelyanov <baralgin38@yandex.ru>
2022-05-07 00:20:44 +0300

    Adds basic validation
    The form is not sent with empty fields

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,15 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TextField from '../components/textField';
 
 const Login = () => {
   const [data, setData] = useState({ email: '', password: '' });
+  const [, setErrors] = useState();
+
+  useEffect(() => {
+    validate();
+  }, [data]);
+
+  const validate = () => {
+    const errors = {};
+    for (const fieldName in data) {
+      if (data[fieldName].trim() === '') {
+        errors[fieldName] = `${fieldName} - is required`;
+      }
+    }
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
 
   const handleChange = ({ target }) => {
     setData((prevState) => ({ ...prevState, [target.name]: target.value }));
   };
 
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const isValidate = validate();
+    if (!isValidate) return;
+    console.log(data);
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
       <TextField
         label="Email"
         name="email"
@@ -23,6 +46,9 @@ const Login = () => {
         value={data.password}
         onChange={handleChange}
       />
+      <button type="submit" className="btn btn-primary btn-sm m-3">
+        Отправить данные
+      </button>
     </form>
   );
 };
