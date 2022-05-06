827b4e0ea4cd1fb4e4c0e3dc4231153f388936b2
Ilya Emelyanov <baralgin38@yandex.ru>
2022-05-06 15:44:53 +0300

    Creates a reusable component with a form field

---
+++ src/app/components/textField.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TextField = ({ label, type, name, value, onChange }) => {
+  return (
+    <div>
+      <label className="m-3" htmlFor="email">
+        {label}
+      </label>
+      <input
+        type={type}
+        id={name}
+        value={value}
+        onChange={onChange}
+        name={name}
+      />
+    </div>
+  );
+};
+
+TextField.defaultProps = {
+  type: 'text'
+};
+
+TextField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+export default TextField;
