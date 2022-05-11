f09935c0c512b2621d5586c049aabf9a798bebad
Ilya Emelyanov <baralgin38@yandex.ru>
2022-05-11 18:49:08 +0300

    Creates a profession selection field

--- src/app/components/page/usersListPage/index.js
+++ src/app/components/page/usersListPage/index.js
@@ -1,2 +1,2 @@
-import UsersListPage from './usersList';
+import UsersListPage from './usersListPage';
 export default UsersListPage;
--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { validator } from '../../utils/validator';
 import TextField from '../common/form/textField';
+import api from '../../api';
 
 const RegisterForm = () => {
-  const [data, setData] = useState({ email: '', password: '' });
+  const [data, setData] = useState({ email: '', password: '', profession: '' });
   const [errors, setErrors] = useState({});
+  const [professions, setProfessions] = useState();
 
   useEffect(() => {
     validate();
   }, [data]);
+
+  useEffect(() => {
+    api.professions.fetchAll().then((data) => setProfessions(data));
+  }, []);
 
   const validate = () => {
     const errors = validator(data, validatorConfig);
@@ -72,6 +78,29 @@ const RegisterForm = () => {
         onChange={handleChange}
         error={errors.password}
       />
+      <div className="mb-4">
+        <label htmlFor="validationCustom04" className="form-label">
+          State
+        </label>
+        <select
+          className="form-select"
+          id="validationCustom04"
+          value={data.profession}
+          onChange={handleChange}
+          name="profession"
+        >
+          <option disabled value="">
+            Choose...
+          </option>
+          {professions &&
+            professions.map((profession) => (
+              <option key={profession._id} value={profession._id}>
+                {profession.name}
+              </option>
+            ))}
+        </select>
+        <div className="invalid-feedback">Please select a valid state.</div>
+      </div>
       <button
         type="submit"
         className="btn btn-primary w-100 mx-auto"
