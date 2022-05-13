c43877337f2fa0ecf672eb31bb70be0161d8a907
Ilya Emelyanov <baralgin38@yandex.ru>
2022-05-13 16:22:05 +0300

    Creates a reusable component with a multiselect

--- src/app/components/common/form/selectField.jsx
+++ src/app/components/common/form/selectField.jsx
@@ -21,6 +21,10 @@ const SelectField = ({
     return `form-select ${error ? 'is-invalid' : ''}`;
   };
 
+  const handleChange = ({ target }) => {
+    onChange({ name: target.name, value: target.value });
+  };
+
   return (
     <div className="mb-4">
       <label htmlFor="validationCustom04" className="form-label">
@@ -30,7 +34,7 @@ const SelectField = ({
         className={getInputClasses()}
         id="validationCustom04"
         value={value}
-        onChange={onChange}
+        onChange={handleChange}
         name="profession"
       >
         <option disabled value="">
--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -4,16 +4,19 @@ import TextField from '../common/form/textField';
 import api from '../../api';
 import SelectField from '../common/form/selectField';
 import RadioField from '../common/form/radioField';
+import MultiSelectField from '../common/form/multiSelectField';
 
 const RegisterForm = () => {
   const [data, setData] = useState({
     email: '',
     password: '',
     profession: '',
-    sex: 'male'
+    sex: 'male',
+    qualities: []
   });
   const [errors, setErrors] = useState({});
   const [professions, setProfessions] = useState();
+  const [qualities, setQualities] = useState({});
 
   useEffect(() => {
     validate();
@@ -21,6 +24,7 @@ const RegisterForm = () => {
 
   useEffect(() => {
     api.professions.fetchAll().then((data) => setProfessions(data));
+    api.qualities.fetchAll().then((data) => setQualities(data));
   }, []);
 
   const validate = () => {
@@ -60,7 +64,7 @@ const RegisterForm = () => {
     }
   };
 
-  const handleChange = ({ target }) => {
+  const handleChange = (target) => {
     setData((prevState) => ({ ...prevState, [target.name]: target.value }));
   };
 
@@ -91,7 +95,7 @@ const RegisterForm = () => {
         error={errors.password}
       />
       <SelectField
-        label="Выберите профессию"
+        label="Выберите вашу профессию"
         value={data.profession}
         onChange={handleChange}
         defaultOption="Choose.."
@@ -106,6 +110,13 @@ const RegisterForm = () => {
         value={data.sex}
         name="sex"
         onChange={handleChange}
+        label="Выберите ваш пол"
+      />
+      <MultiSelectField
+        options={qualities}
+        onChange={handleChange}
+        name="qualities"
+        label="Выберите ваши качества"
       />
       <button
         type="submit"
