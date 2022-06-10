04524d10d48a66a9ea0865593fc970a771b94b94
Ilya Emelyanov <baralgin38@yandex.ru>
2022-06-10 17:14:46 +0300

    Connects the components of comments and user cards

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import api from '../../../api';
 import UserCard from '../../ui/userCard';
+import QualitiesCard from '../../ui/qualitiesCard';
+import MeetingsCard from '../../ui/meetingsCard';
+import Comments from '../../ui/comments';
 
 const UserPage = ({ userId }) => {
   const [user, setUser] = useState();
@@ -13,7 +16,22 @@ const UserPage = ({ userId }) => {
   return (
     <>
       {user ? (
-        <UserCard userData={user} />
+        <div className="container mt-4">
+          <div className="row gutters-sm">
+            <div className="col-md-4 mb-3">
+              <UserCard
+                name={user.name}
+                profession={user.profession.name}
+                rate={user.rate}
+              />
+              <QualitiesCard qualities={user.qualities} />
+              <MeetingsCard meetings={user.completedMeetings} />
+            </div>
+            <div className="col-md-8">
+              <Comments />
+            </div>
+          </div>
+        </div>
       ) : (
         <p className="m-2">Loading user data...</p>
       )}
