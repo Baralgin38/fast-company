16414b6d004c2215ce1ef58a45fc8ca675eec981
Ilya Emelyanov <baralgin38@yandex.ru>
2022-06-10 16:56:13 +0300

    Rewrites the user card component
    Now the component presents the UI in the form of a user card, with an
    avatar, name and rating

--- src/app/components/ui/userCard.jsx
+++ src/app/components/ui/userCard.jsx
@@ -1,46 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Qualities from './qualities';
 import { useHistory } from 'react-router-dom';
+import { createAvatarUrl } from '../../utils/createAvatarUrl';
 
-const UserCard = ({ userData }) => {
+const UserCard = ({ name, profession, rate }) => {
   const history = useHistory();
 
   const editBtnClickHandler = () => {
     history.push(history.location.pathname + '/edit');
   };
 
-  const allUsersBtnClickHandler = () => {
-    history.push('/users');
-  };
-
   return (
-    <>
-      <div className="m-2">
-        <h1>Имя: {userData.name}</h1>
-        <h2>Профессия: {userData.profession.name}</h2>
-        <p>Личные качества: {<Qualities qualities={userData.qualities} />}</p>
-        <p>Встретился, раз: {userData.completedMeetings}</p>
-        <p>Оценка: {userData.rate}</p>
+    <div className="card mb-3">
+      <div className="card-body">
+        <button
+          className="position-absolute top-0 end-0 btn btn-light btn-sm"
+          onClick={editBtnClickHandler}
+        >
+          <i className="bi bi-gear"></i>
+        </button>
+        <div className="d-flex flex-column align-items-center text-center position-relative">
+          <img src={createAvatarUrl()} className="rounded-circle" width="150" />
+          <div className="mt-3">
+            <h4>{name}</h4>
+            <p className="text-secondary mb-1">{profession}</p>
+            <div className="text-muted">
+              <i
+                className="bi bi-caret-down-fill text-primary"
+                role="button"
+              ></i>
+              <i className="bi bi-caret-up text-secondary" role="button"></i>
+              <span className="ms-2">{rate}</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <button
-        className="btn btn-primary btn-sm m-2"
-        onClick={editBtnClickHandler}
-      >
-        Редактировать
-      </button>
-      <button
-        className="btn btn-primary btn-sm m-2"
-        onClick={allUsersBtnClickHandler}
-      >
-        Все пользователи
-      </button>
-    </>
+    </div>
   );
 };
 
 UserCard.propTypes = {
-  userData: PropTypes.object.isRequired
+  name: PropTypes.string.isRequired,
+  profession: PropTypes.string.isRequired,
+  rate: PropTypes.number.isRequired
 };
 
 export default UserCard;
