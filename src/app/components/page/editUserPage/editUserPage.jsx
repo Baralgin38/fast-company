7c48defdddc2c7c4753a4b85a73a315a78beb259
Ilya Emelyanov <baralgin38@yandex.ru>
2022-06-10 17:15:46 +0300

    Adds the BackButton component

--- src/app/components/page/editUserPage/editUserPage.jsx
+++ src/app/components/page/editUserPage/editUserPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import EditUserForm from '../../ui/editUserForm';
+import BackHistoryButton from '../../common/backButton';
 
 const EditUserPage = ({ userId }) => {
   return (
     <div className="container mt-5">
+      <BackHistoryButton />
       <div className="row">
         <div className="col-md-6 offset-md-3 shadow p-4">
           <EditUserForm userId={userId} />
